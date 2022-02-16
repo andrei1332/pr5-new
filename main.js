eb954071cf089254b415d9bc0c5694e2bfee24f6
andrei1332 <90900402+andrei1332@users.noreply.github.com>
2022-02-16 20:23:32 +0200

    pr 5_3

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('.button.action-button')
+const logsWrapper = document.querySelector('#logs')
 
 let isGameFinished = false
 
@@ -11,14 +12,14 @@ const finishGame = () => {
 }
 
 class Pokemon {
-    #name
+    name
     #elHP
     #elProgressbar
     #defaultHP
     #damageHP
 
     constructor(name, elHP, elProgressbar) {
-        this.#name = name
+        this.name = name
         this.#elHP = elHP
         this.#elProgressbar = elProgressbar
         this.#defaultHP = 100
@@ -38,13 +39,28 @@ class Pokemon {
         this.#elProgressbar.style.width = this.#damageHP + '%';
     }
 
-    changeHP(count) {
-        console.log(count);
+    #generateLog({ name }, damaged) {
+        const logs = [
+            `${this.name} вспомнил что-то важное, но неожиданно ${name}, не помня себя от испуга, ударил в предплечье врага. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} поперхнулся, и за это ${name} с испугу приложил прямой удар коленом в лоб врага. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} забылся, но в это время наглый ${name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} пришел в себя, но неожиданно ${name} случайно нанес мощнейший удар. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} поперхнулся, но в это время ${name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} удивился, а ${name} пошатнувшись влепил подлый удар. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} высморкался, но неожиданно ${name} провел дробящий удар. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} пошатнулся, и внезапно наглый ${name} беспричинно ударил в ногу противника -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} расстроился, как вдруг, неожиданно ${name} случайно влепил стопой в живот соперника. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+            `${this.name} пытался что-то сказать, но вдруг, неожиданно ${name} со скуки, разбил бровь сопернику. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
+        ];
 
+        return logs[random(logs.length) - 1]
+    }
+
+    changeHP(count, enemy) {
         if (!isGameFinished) {
             if (this.#damageHP < count) {
                 this.#damageHP = 0;
-                alert(`${this.#name} wasted`)
+                alert(`${this.name} wasted`)
                 finishGame()
             } else {
                 this.#damageHP -= count;
@@ -52,6 +68,8 @@ class Pokemon {
 
             this.renderHP();
         }
+
+        logsWrapper.innerHTML = logsWrapper.innerHTML.concat(`<p>${this.#generateLog(enemy, count)}</p>`)
     }
 }
 
@@ -68,8 +86,8 @@ const Charmander = new Pokemon(
 )
 
 const listenerHandler = (limits) => {
-    Pikachu.changeHP(random(...limits));
-    Charmander.changeHP(random(...limits));
+    Pikachu.changeHP(random(...limits), Charmander);
+    Charmander.changeHP(random(...limits), Pikachu);
 }
 
 Array.from(buttons).forEach((button) => {
