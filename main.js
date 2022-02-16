7f0b99af53d7416a6b26d826456a6d0d676e400e
andrei1332 <90900402+andrei1332@users.noreply.github.com>
2022-02-16 20:26:42 +0200

    pr 5_5

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { Pokemon } from './pokemon.js'
+import { random } from './tools.js'
+
 const buttons = document.querySelectorAll('.button.action-button')
 const logsWrapper = document.querySelector('#logs')
 
@@ -24,68 +27,6 @@ const finishGame = () => {
     })
 }
 
-class Pokemon {
-    name
-    #elHP
-    #elProgressbar
-    #defaultHP
-    #damageHP
-
-    constructor(name, elHP, elProgressbar) {
-        this.name = name
-        this.#elHP = elHP
-        this.#elProgressbar = elProgressbar
-        this.#defaultHP = 100
-        this.#damageHP = 100
-    }
-    
-    renderHP() {
-        this.#renderHPLife();
-        this.#renderProgressbarHP();
-    }
-
-    #renderHPLife() {
-        this.#elHP.innerText = this.#damageHP + ' / ' + this.#defaultHP + ' HP';
-    }
-
-    #renderProgressbarHP() {
-        this.#elProgressbar.style.width = this.#damageHP + '%';
-    }
-
-    #generateLog({ name }, damaged) {
-        const logs = [
-            `${this.name} вспомнил что-то важное, но неожиданно ${name}, не помня себя от испуга, ударил в предплечье врага. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} поперхнулся, и за это ${name} с испугу приложил прямой удар коленом в лоб врага. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} забылся, но в это время наглый ${name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} пришел в себя, но неожиданно ${name} случайно нанес мощнейший удар. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} поперхнулся, но в это время ${name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} удивился, а ${name} пошатнувшись влепил подлый удар. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} высморкался, но неожиданно ${name} провел дробящий удар. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} пошатнулся, и внезапно наглый ${name} беспричинно ударил в ногу противника -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} расстроился, как вдруг, неожиданно ${name} случайно влепил стопой в живот соперника. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-            `${this.name} пытался что-то сказать, но вдруг, неожиданно ${name} со скуки, разбил бровь сопернику. -${damaged}, [${this.#damageHP}/${this.#defaultHP}]`,
-        ];
-
-        return logs[random(logs.length) - 1]
-    }
-
-    changeHP(count, enemy) {
-        if (!isGameFinished) {
-            if (this.#damageHP < count) {
-                this.#damageHP = 0;
-                alert(`${this.name} wasted`)
-                finishGame()
-            } else {
-                this.#damageHP -= count;
-            }
-
-            this.renderHP();
-        }
-
-        logsWrapper.innerHTML = logsWrapper.innerHTML.concat(`<p>${this.#generateLog(enemy, count)}</p>`)
-    }
-}
-
 const Pikachu = new Pokemon(
     'Pikachu',
     document.querySelector('#health-character'),
@@ -99,9 +40,20 @@ const Charmander = new Pokemon(
 )
 
 const listenerHandler = (limits) => {
-    
-    Pikachu.changeHP(random(...limits), Charmander);
-    Charmander.changeHP(random(...limits), Pikachu);
+    if (!isGameFinished) {
+        const pikachuLog = Pikachu.changeHP(random(...limits), Charmander);
+
+        if (Pikachu.isDead) {
+            finishGame()
+        } else {
+            const charmLog = Charmander.changeHP(random(...limits), Pikachu);
+            logsWrapper.innerHTML = logsWrapper.innerHTML.concat(`<p>${pikachuLog}</p>`, `<p>${charmLog}</p>`)
+        }
+
+        if (Charmander.isDead) {
+            finishGame()
+        }
+    }
 }
 
 Array.from(buttons).forEach((button) => {
@@ -115,18 +67,6 @@ Array.from(buttons).forEach((button) => {
         }
     })
 })
-
-const random = (from, to) => {
-    if (Number(from) > Number(to)) {
-        throw Error('Wrong values')
-    }
-
-    return (
-        !to
-            ? Math.ceil(Math.random() * Number(from))
-            : Math.ceil(Math.random() * (Number(to) - Number(from))) + Number(from)
-    )
-}
 
 Pikachu.renderHP()
 Charmander.renderHP()
